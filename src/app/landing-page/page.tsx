a7b55cb5430562cd4c39ebb085d506f85ff0ea27
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-04-13 17:00:32 +0530

    fetching data with client components

---
+++ src/app/landing-page/page.tsx
@@ -0,0 +1,12 @@
+import {Navbar} from "@/app/components/navbar";
+
+export default function LandingPage(){
+    return (
+        <>
+        <Navbar/>
+        <main>
+            <h1>Main Page Heading</h1>
+        </main>
+        </>
+    )
+}
--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,5 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest){
-    return NextResponse.redirect(new URL("/",request.url));
+    //return NextResponse.redirect(new URL("/",request.url));
+
+    if (request.nextUrl.pathname === "/time"){
+        return NextResponse.rewrite(new URL("/",request.nextUrl));
+    }
+
+    
 }
+
+// export const config = {
+//     matcher: "/hello",
+// };
